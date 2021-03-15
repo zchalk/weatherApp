61023296b455986187bc6bc946bf29c81f8a23fa
zchalk <you@example.com>
2021-03-15 10:53:10 -0400

    api calls all work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,44 @@
-
 var apiKey = "f80bb50772c40a878ed5746b10181c83";
-var oneCallAPIURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=" + apiKey;
-console.log(oneCallAPIURL);
-var cityInput = $("input").val();
-var  coordinateAPIURL = "https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=" + apiKey
-
-fetch (coordinateAPIURL)
-.then(function (response) {
-    // return response.json();
-    console.log(response);
-});
-
-var history = [] || JSON.parse(localStorage.getItem("history"));
+let history = JSON.parse(localStorage.getItem("history")) || [];
 var search = [];
 
-$("button.searchButton").on("click", function(event){
+
+function getCoords(city) {
+    var  coordinateAPIURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+fetch (coordinateAPIURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) { 
+    console.log(data.coord.lat,data.coord.lon);
+
+    history.push( {
+        cityName: city,
+        lat: data.coord.lat,
+        lon: data.coord.lon,
+    });
+    console.log(history);
+    getWeather(data.coord.lat,data.coord.lon);
+}); }
+
+function getWeather(lat,lon) {
+    var oneCallAPIURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + apiKey;
+
+fetch (oneCallAPIURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) { 
+    console.log(data);
+});
+}
+
+
+$("#searchButton").on("click", function(){
+    var cityInput = $("input").val();
     search.push(cityInput);
-    GeolocationCoordinates
+    console.log(cityInput);
+    getCoords(cityInput); 
 }); 
 
 
-for (let i = 0; i < search.length; i++) {
-  history.push( {
-      cityName: search[i],
-      lat: "",
-      lon: "",
-})};
-
-// $(history).each(function() {
-// something to add coordinates into history
-// });
