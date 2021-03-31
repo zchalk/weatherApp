1bca232ada74b72674862ab05ba4a3a5c9d1dfab
zoë <zoe@zoes-mbp.home>
2021-03-31 18:10:11 -0400

    more css

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var main = $("main");
 var historyStorage = $("#historyStorage");
 var todayIconBarrier = $("#todayIconBarrier");
 var todayContainer = $("#todayContainer");
+var todayContainer2 = $("#todayContainer2");
 var futureContainer = $("#futureContainer");
 
 
@@ -28,7 +29,7 @@ fetch (coordinateAPIURL)
 }
 
 function getWeather(lat,lon) {
-    var oneCallAPIURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&units={imperial}&appid=" + apiKey;
+    var oneCallAPIURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apiKey;
 
 fetch (oneCallAPIURL)
 .then(function (response) {
@@ -47,24 +48,53 @@ var unixSeconds = new Date(unixTime * 1000);
 var date = (new Date(unixSeconds).toLocaleDateString("en-US"));
 $("#date").text(date);
 var icon = weatherData.current.weather[0].icon;
-$("#temp").text(weatherData.current.temp);
-$("#humidity").text(weatherData.current.humidity);
-$("#windSpeed").text(weatherData.current.wind_speed);
+$("#temp").text(weatherData.current.temp + "°");
+$("#humidity").text(weatherData.current.humidity + "% humidity");
+$("#windSpeed").text(weatherData.current.wind_speed + "mph");
 $("#UV").text(weatherData.current.uvi);
 $("#icon").attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
-console.log(icon);
+UVindex();
 };
 
 function template() {
     var icon = $("<img>").attr("id", "icon");
-    todayIconBarrier.append(icon);
+    todayIconBarrier.prepend(icon);
     var city = $("<h2>").attr("id", "city");
     var date = $("<h3>").attr("id", "date");
+    var tempContainer = $("<div>").attr("id", "tempContainer");
+    todayContainer.prepend(city,date,tempContainer);    
     var temp = $("<p>").attr("id", "temp");
+    var tempIcon = $("<img>").attr({id:"tempIcon", src:"./assets/images/sunnyTemp.png"});
+    tempContainer.prepend(tempIcon,temp);
+    var humidityContainer = $("<div>").attr("id", "humidityContainer");
+    var windSpeedContainer = $("<div>").attr("id", "windSpeedContainer");
+    var UVcontainer = $("<div>").attr("id","UVcontainer");
+    todayContainer2.append(humidityContainer,windSpeedContainer,UVcontainer);
+    var humidityIcon = $("<img>").attr({id:"humidityIcon", src:"./assets/images/humidity.png"});
     var humidity = $("<p>").attr("id", "humidity");
+    humidityContainer.append(humidityIcon, humidity);
     var windSpeed = $("<p>").attr("id", "windSpeed");
+    windSpeedContainer.append(windSpeed);
     var UV = $("<p>").attr("id", "UV");
-    todayContainer.append(city,date,temp,humidity,windSpeed,UV);
+    UVcontainer.append(UV);
+};
+
+function UVindex() {
+    var UV = $('#UV').html();
+    let UVelement = $('#UV');
+    console.log(UVelement);
+    console.log(UV);
+    if (UV < 3) {
+        UVelement.addClass('low');
+    } else if (3 < UV < 6) {
+        UVelement.addClass('moderate');
+    } else if (6 < UV < 8) {
+        UVelement.addClass('high');
+    } else if (8 < UV < 11) {
+        UVelement.addClass('veryHigh');
+    } else if (UV > 11) {
+        UVelement.addClass('extreme');
+    }
 };
 
 $("#searchButton").on("click", function(){
